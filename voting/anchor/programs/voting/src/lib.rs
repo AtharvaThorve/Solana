23f0aa2ca773e6fefb1cdff7c4e764868ef5b207
Atharva Thorve <aaathorve@gmail.com>
2025-02-21 22:32:07 -0500

    Enhance initialize_poll function to include description, start, and end times for polls

--- voting/anchor/programs/voting/src/lib.rs
+++ voting/anchor/programs/voting/src/lib.rs
@@ -10,37 +10,48 @@ pub const ANCHOR_DISCRIMINATOR_SIZE: usize = 8;
 pub mod voting {
     use super::*;
 
-    pub fn initialize_poll(_ctx: Context<InitializePoll>, _poll_id: u64) -> Result<()> {
-      Ok(())
+    pub fn initialize_poll(
+        ctx: Context<InitializePoll>,
+        poll_id: u64,
+        description: String,
+        poll_start: u64,
+        poll_end: u64,
+    ) -> Result<()> {
+        let poll = &mut ctx.accounts.poll;
+        poll.poll_id = poll_id;
+        poll.description = description;
+        poll.poll_start = poll_start;
+        poll.poll_end = poll_end;
+        poll.candidate_amount = 0;
+        Ok(())
     }
 }
-
 
 #[derive(Accounts)]
 #[instruction(poll_id: u64)]
 pub struct InitializePoll<'info> {
-  #[account(mut)]
-  pub signer: Signer<'info>,
+    #[account(mut)]
+    pub signer: Signer<'info>,
 
-  #[account(
+    #[account(
     init,
     payer = signer,
     space = ANCHOR_DISCRIMINATOR_SIZE + Poll::INIT_SPACE,
     seeds = [poll_id.to_le_bytes().as_ref()],
     bump,
   )]
-  pub poll: Account<'info, Poll>,
+    pub poll: Account<'info, Poll>,
 
-  pub system_program: Program<'info, System>,
+    pub system_program: Program<'info, System>,
 }
 
 #[account]
 #[derive(InitSpace)]
 pub struct Poll {
-  pub poll_id: u64,
-  #[max_len(280)]
-  pub description: String,
-  pub poll_start: u64,
-  pub poll_end: u64,
-  pub candidate_amount: u64,
+    pub poll_id: u64,
+    #[max_len(280)]
+    pub description: String,
+    pub poll_start: u64,
+    pub poll_end: u64,
+    pub candidate_amount: u64,
 }
