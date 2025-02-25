8524c98d8f1b3f53122c0dc02c9b69ce24cc7224
Atharva Thorve <aaathorve@gmail.com>
2025-02-24 23:14:52 -0500

    Add initialize_candidate function and Candidate struct to support candidate registration

--- voting/anchor/programs/voting/src/lib.rs
+++ voting/anchor/programs/voting/src/lib.rs
@@ -25,6 +25,49 @@ pub mod voting {
         poll.candidate_amount = 0;
         Ok(())
     }
+
+    pub fn initialize_candidate(
+        ctx: Context<IntitializeCandidate>,
+        candidate_name: String,
+        _poll_id: u64,
+    ) -> Result<()> {
+        let candidate = &mut ctx.accounts.candidate;
+        candidate.candidate_name = candidate_name;
+        candidate.candidate_votes = 0;
+        Ok(())
+    }
+}
+
+#[derive(Accounts)]
+#[instruction(candidate_name: String, poll_id: u64)]
+pub struct IntitializeCandidate<'info> {
+    #[account(mut)]
+    pub signer: Signer<'info>,
+
+    #[account(
+        seeds = [poll_id.to_le_bytes().as_ref()],
+        bump,
+    )]
+    pub poll: Account<'info, Poll>,
+
+    #[account(
+        init,
+        payer = signer,
+        space = ANCHOR_DISCRIMINATOR_SIZE + Candidate::INIT_SPACE,
+        seeds = [poll_id.to_le_bytes().as_ref(), candidate_name.as_bytes()],
+        bump,
+    )]
+    pub candidate: Account<'info, Candidate>,
+
+    pub system_program: Program<'info, System>,
+}
+
+#[account]
+#[derive(InitSpace)]
+pub struct Candidate {
+    #[max_len(32)]
+    pub candidate_name: String,
+    pub candidate_votes: u64,
 }
 
 #[derive(Accounts)]
@@ -34,12 +77,12 @@ pub struct InitializePoll<'info> {
     pub signer: Signer<'info>,
 
     #[account(
-    init,
-    payer = signer,
-    space = ANCHOR_DISCRIMINATOR_SIZE + Poll::INIT_SPACE,
-    seeds = [poll_id.to_le_bytes().as_ref()],
-    bump,
-  )]
+        init,
+        payer = signer,
+        space = ANCHOR_DISCRIMINATOR_SIZE + Poll::INIT_SPACE,
+        seeds = [poll_id.to_le_bytes().as_ref()],
+        bump,
+    )]
     pub poll: Account<'info, Poll>,
 
     pub system_program: Program<'info, System>,
