0744e1d0211f11c32f8c292eb77c1048dd3001ad
Atharva Thorve <aaathorve@gmail.com>
2025-02-25 01:02:28 -0500

    Add logging for vote function and update account attributes for candidate and poll

--- voting/anchor/programs/voting/src/lib.rs
+++ voting/anchor/programs/voting/src/lib.rs
@@ -43,6 +43,8 @@ pub mod voting {
     pub fn vote(ctx: Context<Vote>, _candidate_name: String, _poll_id: u64) -> Result<()> {
         let candidate = &mut ctx.accounts.candidate;
         candidate.candidate_votes += 1;
+        msg!("Voted for candidate: {}", candidate.candidate_name);
+        msg!("Votes: {}", candidate.candidate_votes);
         Ok(())
     }
 }
@@ -59,6 +61,7 @@ pub struct Vote<'info> {
     pub poll: Account<'info, Poll>,
 
     #[account(
+        mut,
         seeds = [poll_id.to_le_bytes().as_ref(), candidate_name.as_bytes()],
         bump,
     )]
@@ -72,6 +75,7 @@ pub struct IntitializeCandidate<'info> {
     pub signer: Signer<'info>,
 
     #[account(
+        mut,
         seeds = [poll_id.to_le_bytes().as_ref()],
         bump,
     )]
