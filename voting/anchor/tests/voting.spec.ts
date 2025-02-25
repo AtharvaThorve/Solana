4b20eba68b04b646b39f4f21c097c2ecc342f211
Atharva Thorve <aaathorve@gmail.com>
2025-02-24 22:27:24 -0500

    Add poll description, start, and end time fields to voting structure and tests

--- voting/anchor/tests/voting.spec.ts
+++ voting/anchor/tests/voting.spec.ts
@@ -1,76 +1,49 @@
-import * as anchor from '@coral-xyz/anchor'
-import {Program} from '@coral-xyz/anchor'
-import {Keypair} from '@solana/web3.js'
-import {Voting} from '../target/types/voting'
+import * as anchor from "@coral-xyz/anchor";
+import { Program } from "@coral-xyz/anchor";
+import { PublicKey } from "@solana/web3.js";
+import { BankrunProvider, startAnchor } from "anchor-bankrun";
+import { Voting } from "../target/types/voting";
 
-describe('voting', () => {
-  // Configure the client to use the local cluster.
-  const provider = anchor.AnchorProvider.env()
-  anchor.setProvider(provider)
-  const payer = provider.wallet as anchor.Wallet
+const IDL = require("../target/idl/voting.json");
 
-  const program = anchor.workspace.Voting as Program<Voting>
+const votingAddress = new PublicKey(
+    "coUnmi3oBUtwtd9fjeAvSsJssXh5A5xyPbhpewyzRVF"
+);
 
-  const votingKeypair = Keypair.generate()
+describe("voting", () => {
+    it("Initialize Poll", async () => {
+        const context = await startAnchor(
+            "",
+            [{ name: "voting", programId: votingAddress }],
+            []
+        );
 
-  it('Initialize Voting', async () => {
-    await program.methods
-      .initialize()
-      .accounts({
-        voting: votingKeypair.publicKey,
-        payer: payer.publicKey,
-      })
-      .signers([votingKeypair])
-      .rpc()
+        const provider = new BankrunProvider(context);
 
-    const currentCount = await program.account.voting.fetch(votingKeypair.publicKey)
+        const votingProgram = new Program<Voting>(IDL, provider);
 
-    expect(currentCount.count).toEqual(0)
-  })
+        await votingProgram.methods
+            .initializePoll(
+                new anchor.BN(1),
+                "What is your favorite type of peanut butter?",
+                new anchor.BN(0),
+                new anchor.BN(1840195777)
+            )
+            .rpc();
 
-  it('Increment Voting', async () => {
-    await program.methods.increment().accounts({ voting: votingKeypair.publicKey }).rpc()
+        const [pollAddress] = PublicKey.findProgramAddressSync(
+            [new anchor.BN(1).toArrayLike(Buffer, "le", 8)],
+            votingAddress
+        );
 
-    const currentCount = await program.account.voting.fetch(votingKeypair.publicKey)
+        const poll = await votingProgram.account.poll.fetch(pollAddress);
 
-    expect(currentCount.count).toEqual(1)
-  })
+        console.log(poll);
 
-  it('Increment Voting Again', async () => {
-    await program.methods.increment().accounts({ voting: votingKeypair.publicKey }).rpc()
-
-    const currentCount = await program.account.voting.fetch(votingKeypair.publicKey)
-
-    expect(currentCount.count).toEqual(2)
-  })
-
-  it('Decrement Voting', async () => {
-    await program.methods.decrement().accounts({ voting: votingKeypair.publicKey }).rpc()
-
-    const currentCount = await program.account.voting.fetch(votingKeypair.publicKey)
-
-    expect(currentCount.count).toEqual(1)
-  })
-
-  it('Set voting value', async () => {
-    await program.methods.set(42).accounts({ voting: votingKeypair.publicKey }).rpc()
-
-    const currentCount = await program.account.voting.fetch(votingKeypair.publicKey)
-
-    expect(currentCount.count).toEqual(42)
-  })
-
-  it('Set close the voting account', async () => {
-    await program.methods
-      .close()
-      .accounts({
-        payer: payer.publicKey,
-        voting: votingKeypair.publicKey,
-      })
-      .rpc()
-
-    // The account should no longer exist, returning null.
-    const userAccount = await program.account.voting.fetchNullable(votingKeypair.publicKey)
-    expect(userAccount).toBeNull()
-  })
-})
+        expect(poll.pollId.toNumber()).toEqual(1);
+        expect(poll.description).toEqual(
+            "What is your favorite type of peanut butter?"
+        );
+        expect(poll.pollStart.toNumber()).toBeLessThan(poll.pollEnd.toNumber());
+    });
+});
